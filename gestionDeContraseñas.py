b2b44d6857bfb174ef9489c513aedc0e51d86c7c
Ariel Robotti <49838363+ArielRobotti@users.noreply.github.com>
2019-12-12 23:46:35 -0300

    Update gestionDeContraseñas.py
    En esta actualización se modificaron los tamaños y la distribución de los elementos de la interfaz y se eliminó el campo "otros". También fue optimizada la función de búsqueda y se estableció un botón de cargado del registro seleccionado en la lista desplegable. La opción buscar del menú quedó resultaba innecesaria así que fue eliminada. Queda pendiente la implementación de un botón de generación aleatoria de claves y finalmente el encriptación y guardado permanente de los registros. Para probar las distintas funciones, coloqué un texto en base al cual se generaron unos 120 registros a los que se puede agregar otros nuevos ingresados mediante la opción guardar del menú archivo.

--- gestionDeContraseñas.py
+++ gestionDeContraseñas.py
@@ -9,10 +9,13 @@ from tkinter import messagebox as mb
 from time import *
 
 raiz=Tk()
-raiz.geometry("640x300")
+raiz.geometry("610x300")
 raiz.title("Gestor de contraseñas 1.0")
 raiz.iconbitmap("llaves.ico")
-raiz.resizable(0,0)
+#raiz.resizable(0,0)
+
+barraBuscar=Frame(raiz)
+barraBuscar.grid(row=7,column=1,columnspan=3,padx=2,pady=6)
 
 resultados=[]
 grupo=[]
@@ -27,6 +30,7 @@ barraMenu=Menu(raiz)
 raiz.config(menu=barraMenu)
 
 def guardarContraseña():
+	global grupo
 	bandera=True
 	url=varUrl.get()
 	email=varEmail.get()
@@ -46,35 +50,49 @@ def guardarContraseña():
 				grupo.append(Contraseña(url,email,contraseña,usuario,observaciones))
 				info.set("Registro ingresado correctamente: Actualmente existen "+str(len(grupo))+" registros en el archivo.")
 			else:
-				info.set("Error al cargar: Dirección Url no válida")
+				info.set("Error al cargar: Dirección Url no válida...pendiente: quitar color rojo al comenzar a escribir")
 				dispUrl.config(bg="red")			
 		else:
-			info.set("Error al cargar:  Email no válido")
+			info.set("Error al cargar:  Email no válido... pendiente: quitar color rojo al comenzar a escribir")
 			dispMail.config(bg="red")
 def salir():
 	respuesta= mb.askyesno("Cuidado", "¿Quiere salir del programa?")
 	if respuesta==True:
 		raiz.destroy()
 
-
 def buscar():
 	global grupo
+	global listResult
 	resultados=[]
-	entrada=aBuscar.get()		
-	for i in range (len(grupo)):
-		if entrada in grupo[i].url or entrada in grupo[i].email:
-			resultados.append((grupo[i].url,'/',grupo[i].email))
-	print("resultados: ",resultados)
-	cantidadRes=len(resultados)
-	if cantidadRes!=0:
-		varLista.set(resultados[0])
-		listResult=OptionMenu(barraBuscar,varLista,*resultados).grid(row=1,column=3)
-		if cantidadRes==1:
-			info.set("Se obtuvo 1 resultado para su busqueda")
-		else:
-			info.set("Se obtuvieron "+str(cantidadRes)+" resultados para su busqueda")
+	entrada=aBuscar.get()
+	if entrada=="":
+		info.set("Escriba lo que quiere buscar o escriba * para ver todo")
 	else:
-		info.set("No hay resultados para la busqueda")
+		if entrada=="*":		#si el usuario quere ver todo
+			entrada=""			#la cadena vacia devuelve como resultado todos los elementos de la lista
+
+		for i in range (len(grupo)):
+			if entrada in grupo[i].url or entrada in grupo[i].email:
+				resultados.append([i,grupo[i].url,"/",grupo[i].email])
+
+		cantidadRes=len(resultados)
+		if cantidadRes!=0:
+			varLista.set("Seleccione un resultado y presione el boton cargar")
+			listResult.destroy()
+			listResult=OptionMenu(barraBuscar,varLista,*resultados)
+			listResult.grid(row=1,column=3)
+			listResult.config(width=42)
+			if cantidadRes==1:
+				info.set("Se obtuvo 1 resultado para su busqueda")
+			else:
+				info.set("Se obtuvieron "+str(cantidadRes)+" resultados para su busqueda")
+		else:
+			listResult.destroy()
+			varLista.set("No se encontraron resultados")
+			listResult=OptionMenu(barraBuscar,varLista,"")
+			listResult.grid(row=1,column=3)
+			listResult.config(width=42)
+			info.set("No hay resultados para la busqueda")
 
 menuArchivo=Menu(barraMenu,tearoff=0,activeborderwidth=4)
 barraMenu.add_cascade(label="Archivo",menu=menuArchivo)
@@ -92,15 +110,10 @@ menuEdicion.add_command(label="Deshacer")
 menuEdicion.add_command(label="Rehacer")
 menuEdicion.add_command(label="Copiar Todo")
 
-menuBuscar=Menu(barraMenu,tearoff=0)
-barraMenu.add_cascade(label="Buscar",menu=menuBuscar)
-menuBuscar.add_command(label="Buscar registro",command=partial(buscar))
-
 menuHerramientas=Menu(barraMenu,tearoff=0)
 barraMenu.add_cascade(label="Herramientas",menu=menuHerramientas)
 herramConfig=Menu(menuHerramientas,tearoff=0)
 menuHerramientas.add_command(label="Configuracion")
-
 
 menuAyuda=Menu(barraMenu,tearoff=0)
 barraMenu.add_cascade(label="Ayuda",menu=menuAyuda)
@@ -111,12 +124,16 @@ menuAyuda.add_command(label="Acerca de")
 def nuevaContraseña(url,email,contraseña,usuario="Desconocido",observaciones="Ninguna"):
 	pass
 
-def mostrarRegistro(contraseña):
-	varUrl.set(contraseña.url)
-	varEmail.set(contraseña.email)
-	varContraseña.set(contraseña.contraseña)
-	varUsuario.set(contraseña.usuario)
-	varObserv.set(contraseña.observaciones)
+def mostrarRegistro():
+
+	indice=int(varLista.get().replace(",","    ")[1:5])
+	contraseñaSel=grupo[indice]
+
+	varUrl.set(contraseñaSel.url)
+	varEmail.set(contraseñaSel.email)
+	varContraseña.set(contraseñaSel.contraseña)
+	varUsuario.set(contraseñaSel.usuario)
+	varObserv.set(contraseñaSel.observaciones)
 
 def copiar(que):
 	aClipBoard=que.get()
@@ -131,82 +148,93 @@ def mostrarContraseña():
 	global dispContr
 	if dispContr.cget("show")=="*":
 		dispContr.config(show="")
-		verContr.config(text="ocultar")		
+		verContr.config(text="ocultar")
+		info.set("Se recomienda mantener la contraseña oculta: usted puede estar siendo vigilado")
 	else:
 		dispContr.config(show="*")
-		verContr.config(text="ver")	
+		verContr.config(text="mostrar")	
+		info.set("Asi esta mejor :)")	
 
-prueba=Contraseña("youtube.com","changocardenas@gmail.com","rupertoElGoma","gual_disnei")
-mostrarRegistro(prueba)
+for i in ("el","siguiente","texto","tiene","la","finalidad","de","generar","registros","para","probar","el","funcionamiento","de","la","aplicacio","La",
+			"revolución", "de",  "Data", "no" ,"solo" ,"se", "refiere", "al" ,"exponencial", "crecimiento", "del", "crecimiento", 
+			"de", "los", "datos", "también", "recae", "en", "el", "mejoramiento", "de" ,"los", "métodos", "estadísticos", "y", "computacionales",
+			"La", "capacidad" ,"de", "cómputo", "se" ,"dobla", "cada", "18", "meses", "según", "la" ,"Ley", "de" ,"Moore", "pero" ,"eso", "es", "nada", "a", 
+			"comparación", "de", "un", "algoritmo", "con", "una", "serie" ,"de", "reglas" ,"que", "puede", "ser", "usado", "para", "resolver" ,"un", 
+			"problema", "miles" ,"de" ,"veces", "más", "rápido" ,"que" ,"un", "método", "computacional", "convencional", "Shaw" ,"2014", 
+			"He" ,"aquí" ,"la" ,"importancia", "en", "el", "mundo" ,"académico","y","para","rellenar","un","poco","mas","acá","agrego","otro","poco","de",
+			"texto","a","ver","si","el","cargador","de","contraseñas","funciona","bien","con","tres","digitos"):
+	grupo.append(Contraseña(i+".com",i[2:]+i[:2]+"@gmail.com","rupertoElGoma","gual_disnei","todo bien"))
 
 imgFondo=PhotoImage(file="llaves.png")
 fondo=Label(raiz,image=imgFondo).grid(row=2,column=1,rowspan=5)
 
-
 labUrl=Label(raiz,width=15,text="Url de la Pagina",font=("Arial",13))
 labUrl.grid(row=2,column=1)
-dispUrl=Entry(raiz,width=25,font=("Arial",18),textvariable=varUrl)
-dispUrl.grid(row=2,column=2)
-copiUrl=Button(raiz,width=5,text="copiar",font=("Arial",14),command=partial(copiar,varUrl))
-copiUrl.grid(row=2,column=3,sticky="E",pady=2)
+dispUrl=Entry(raiz,width=30,font=("Arial",15),textvariable=varUrl)
+dispUrl.grid(row=2,column=2,sticky="W")
+copiUrl=Button(raiz,width=5,text="copiar",font=("Arial",13),command=partial(copiar,varUrl))
+copiUrl.grid(row=2,column=3,padx=10,sticky="E")
 
 labMail=Label(raiz,width=15,text="Email registrado",font=("Arial",13))
 labMail.grid(row=3,column=1)
-dispMail=Entry(raiz,width=25,font=("Arial",18),textvariable=varEmail)
-dispMail.grid(row=3,column=2)
-copiMail=Button(raiz,width=5,text="copiar",font=("Arial",14),command=partial(copiar,varEmail))
-copiMail.grid(row=3,column=3,sticky="E",pady=2)
+dispMail=Entry(raiz,width=30,font=("Arial",15),textvariable=varEmail)
+dispMail.grid(row=3,column=2,sticky="W")
+copiMail=Button(raiz,width=5,text="copiar",font=("Arial",13),command=partial(copiar,varEmail))
+copiMail.grid(row=3,column=3,padx=10,sticky="E")
 
-labUsu=Label(raiz,width=15,text="Nombre de Usuario",font=("Arial",13))
+labUsu=Label(raiz,width=15,text="Nombre Usuario",font=("Arial",13))
 labUsu.grid(row=4,column=1)
-dispUsu=Entry(raiz,width=25,font=("Arial",18),textvariable=varUsuario)
-dispUsu.grid(row=4,column=2)
-copiUsu=Button(raiz,width=5,text="copiar",font=("Arial",14),command=partial(copiar,varUsuario))
-copiUsu.grid(row=4,column=3,sticky="E",pady=2)
+dispUsu=Entry(raiz,width=30,font=("Arial",15),textvariable=varUsuario)
+dispUsu.grid(row=4,column=2,sticky="W")
+copiUsu=Button(raiz,width=5,text="copiar",font=("Arial",13),command=partial(copiar,varUsuario))
+copiUsu.grid(row=4,column=3,padx=10,sticky="E")
 
 labContr=Label(raiz,width=15,text="Contraseña",font=("Arial",13))
 labContr.grid(row=5,column=1)
-dispContr=Entry(raiz,width=24,font=("Arial",14),textvariable=varContraseña,show="*")
+dispContr=Entry(raiz,width=25,font=("Arial",14),textvariable=varContraseña,show="*")
 dispContr.grid(row=5,column=2,sticky="W")
-verContr=Button(raiz,width=5,text="ver",font=("Arial Black",10),command=partial(mostrarContraseña))
+verContr=Button(raiz,width=6,text="mostrar",font=("Arial Black",10),command=partial(mostrarContraseña))
 verContr.grid(row=5,column=2,sticky="E")
-copiContr=Button(raiz,width=5,text="copiar",font=("Arial",14),command=partial(copiar,varContraseña))
-copiContr.grid(row=5,column=3,sticky="E")
+copiContr=Button(raiz,width=5,text="copiar",font=("Arial",13),command=partial(copiar,varContraseña))
+copiContr.grid(row=5,column=3,padx=10,sticky="E")
 
 labExtra=Label(raiz,width=15,text="Observaciones",font=("Arial",13))
 labExtra.grid(row=6,column=1)
-dispExtra=Entry(raiz,width=25,font=("Arial",18),textvariable=varObserv).grid(row=6,column=2)
-copiExtra=Button(raiz,width=5,text="pegar",font=("Arial",14),command=partial(pegar,varObserv))
-copiExtra.grid(row=6,column=3,sticky="E",pady=2)
+dispExtra=Entry(raiz,width=30,font=("Arial",14),textvariable=varObserv)
+dispExtra.grid(row=6,column=2,sticky="W")
+copiExtra=Button(raiz,width=5,text="pegar",font=("Arial",13),command=partial(pegar,varObserv))
+copiExtra.grid(row=6,column=3,padx=10,sticky="E")
 
-
+#--------------------barra de busqueda-----------------------
 aBuscar=StringVar()
-barraBuscar=Frame(raiz)
-barraBuscar.grid(row=7,column=1,columnspan=2)
-
-entrada=Entry(barraBuscar,width=15,font=(13),textvariable=aBuscar)
-entrada.grid(row=1,column=2,sticky="E")
-
 varLista=StringVar(barraBuscar)
-varLista.set("					")
-
-listResult=OptionMenu(barraBuscar,varLista,resultados)
-listResult.grid(row=1,column=3)
 
 botonBuscar=Button(barraBuscar,width=7,text="Buscar",bg="#CCCCCC")
 botonBuscar.grid(row=1,column=1,sticky="W")
 botonBuscar.config(command=partial(buscar))
 
-botonRandom=Button(raiz,width=11,text="NuevaClave",font=("Arial Black",10),bg="#AA8888")
-botonRandom.grid(row=7,column=3)
+entrada=Entry(barraBuscar,width=18,font=(13),textvariable=aBuscar,bg="#DDDDDD")
+entrada.grid(row=1,column=2,sticky="E")
+
+listResult=OptionMenu(barraBuscar,varLista,resultados)
+listResult.grid(row=1,column=3)
+listResult.config(width=42)
+
+botonCargar=Button(barraBuscar,width=7,text="Cargar",bg="#CCCCCC")
+botonCargar.grid(row=1, column=4)
+botonCargar.config(command=partial(mostrarRegistro))
+
+#-------------------------------------------------------------
 
 campoInfo=Frame(raiz)
-campoInfo.grid(row=8,column=1,pady=10,columnspan=3)
+campoInfo.grid(row=8,column=1,padx=10,columnspan=3)
 infoLabel0=Label(campoInfo,width=4,font=("Arial",10),text=" Info: ",bg="#999999")
 infoLabel0.pack(side="left")
 
 infoLabel=Label(campoInfo,width=67,font=("Arial",10),textvariable=info,justify="left",bg="#999999")
-infoLabel.pack(side="right",expand=True)	
+infoLabel.pack(side="right",expand=True)
+
+# botonRandom=Button(raiz,width=11,text="NuevaClave",font=("Arial Black",10),bg="#AA8888")
+# botonRandom.grid(row=7,column=3)
 
 raiz.mainloop()
-
