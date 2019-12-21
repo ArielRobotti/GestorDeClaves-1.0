514d46b4a4ae0ada4df90a620e65a9642457c79c
Ariel Robotti <49838363+ArielRobotti@users.noreply.github.com>
2019-12-21 01:22:02 -0300

    Update claseContr.py

--- claseContr.py
+++ claseContr.py
@@ -4,18 +4,19 @@ class Contraseña():
 	contraseña=""
 	usuario=""
 	observaciones=""
+	historial=[]
 
-	def __init__(self,_URL=None,_Email=None,_Contraseña=None,_usuario=None,_observaciones=None):
+	def __init__(self,_URL="",_Email="",_Contraseña="",_usuario="",_observaciones=""):
 		self.url=_URL
 		self.email=_Email
 		self.contraseña=_Contraseña
 		self.usuario=_usuario
 		self.observaciones=_observaciones
-
+	
+		
 
 	def mostrar(self):
-		return[self.url,self.email,self.contraseña,self.usuario,self.observaciones]
-
+		return[self.url,self.email,self.contraseña,self.usuario,self.observaciones,self.historial]
 
 
 
